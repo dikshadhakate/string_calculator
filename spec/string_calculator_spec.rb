3457898e2e005c525597810204372eb27bff8948
diksha_dhakate <diksha_dhakate@persistent.com>
2024-12-21 13:08:30 +0530

    returns the sum of multiple numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require_relative '../string_calculator'
 
 describe '#add' do
-	it 'returns the sum of two numbers' do
-		expect(add("5,4")).to eq(9)
+	it 'returns the sum of multiple numbers' do
+		expect(add("1\n2,3")).to eq(6)
 	end	
 end
