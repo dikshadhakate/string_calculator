ff5b0f3c77061fac393e2439b3d2510a785b8747
diksha_dhakate <diksha_dhakate@persistent.com>
2024-12-21 13:10:22 +0530

    supports custom delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,7 +2,7 @@ require 'rspec'
 require_relative '../string_calculator'
 
 describe '#add' do
-	it 'returns the sum of multiple numbers' do
-		expect(add("1\n2,3")).to eq(6)
+	it 'supports custom delimiter' do
+		expect(add("//;\n1;2")).to eq(3)
 	end	
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,12 @@
 #This method is use to perform the addition operation on string numbers
 def add(string_numbers)
 	return 0 if string_numbers.empty?
-	string_numbers.split(/,|\n/).map(&:to_i).sum
+	
+	if string_numbers.start_with?("//")
+		delimiter = string_numbers[2]
+		string_numbers = string_numbers[4..]
+		string_numbers.split(/#{delimiter}|\n/).map(&:to_i).sum
+	else
+		string_numbers.split(/,|\n/).map(&:to_i).sum
+	end
 end
